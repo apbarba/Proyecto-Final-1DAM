ba277261d6587fbaf9232adbb7457e1200157beb
Ana Pilar Barba López <pilarbarba03@gmail.com>
2022-05-11 12:33:49 +0200

    Siguiendo con el proyecto

--- RosarioDeCadiz/src/main/java/com/salesianostriana/dam/controller/ProductosController.java
+++ RosarioDeCadiz/src/main/java/com/salesianostriana/dam/controller/ProductosController.java
@@ -1,14 +1,26 @@
 package com.salesianostriana.dam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.salesianostriana.dam.model.ProductosModel;
 
 @Controller
 public class ProductosController {
 	
-	@GetMapping 
-	public String productosBanda() {
+	@GetMapping ("/productos")
+	public String productosBanda(Model model) {
+		
+		model.addAttribute("productos", new ProductosModel("Camiseta Rosario",100, 23.50, true,1 ));
 		
 		return "Productos"; //Nombre de la página en la que se verá al igual que venta
 	}
 }
+
+
+//private String nombre_Producto;
+//private int cantidad;
+//private double precio;
+//private boolean disponibilidad;
+//private long id_producto;
--- RosarioDeCadiz/src/main/java/com/salesianostriana/dam/model/ProductosModel.java
+++ RosarioDeCadiz/src/main/java/com/salesianostriana/dam/model/ProductosModel.java
@@ -2,12 +2,21 @@ package com.salesianostriana.dam.model;
 
 public class ProductosModel {
 
-	private int tipos;
+
 	private String nombre_Producto;
 	private int cantidad;
 	private double precio;
-	private double descuento;
 	private boolean disponibilidad;
-	private int id_producto;
+	private long id_producto;
+	
+	public ProductosModel(String nombre_Producto, int cantidad, double precio, boolean disponibilidad,
+			long id_producto) {
+		super();
+		this.nombre_Producto = nombre_Producto;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.disponibilidad = disponibilidad;
+		this.id_producto = id_producto;
+	}
 	
 }
